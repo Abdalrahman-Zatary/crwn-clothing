717cc27c580b2bf290e32602ebd826467fe279ae
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-01-15 19:27:16 +0300

    style: add sass to category-container and category-item

--- src/App.js
+++ src/App.js
@@ -1,42 +1,52 @@
+import './categories.styles.scss';
 
 const App = () => {
 
   const categories = [
     {
-      id: '1',
-      title: 'Hats',
+      id: 1,
+      title: "Hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
-      id: '2',
-      title: 'Jackets',
+      id: 2,
+      title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
-      id: '3',
-      title: 'Sneakers',
+      id: 3,
+      title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
-      id: '4',
-      title: 'Womens',
+      id: 4,
+      title: "Womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
-      id: '5',
-      title: 'Mens',
+      id: 5,
+      title: "Mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
   return (
-    <div className='categories-container'>
-      {
-        categories.map(({title}) => (
-          <div className='category'>
-            <div className='background-image' /> 
-            <div className='category-content'>
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
+    <div className="categories-container">
+      {categories.map(({ title, id, imageUrl }) => (
+        <div key={id} className="category">
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          />
+          <div className="category-content">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
           </div>
-        ))
-      };
+        </div>
+      ))}
+      ;
     </div>
   );
 }
