0c428eaf2d9383f5fba2344274e6fb96f21650e6
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-01-30 22:44:36 +0300

    feat: enhance Cart functionality with cartCount and CartItem styling

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const addCartItem = (cartItem, productToAdd) => {
   // find if cartItems contains productToAdd
@@ -24,18 +24,25 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItem: [],
   addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItem, setCartItem] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const newCartCount = cartItem.reduce((total, cartItem) => total + cartItem.quantity, 0)
+    setCartCount(newCartCount)
+  }, [cartItem]);
 
   const addItemToCart = (productToAdd) => {
     setCartItem(addCartItem(cartItem, productToAdd));
   }
 
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItem };
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItem, cartCount };
 
   return (
     <CartContext.Provider value={value}>{ children }</CartContext.Provider>
