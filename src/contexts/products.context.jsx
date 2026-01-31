014d18e2b2ffed13e989391ffae84c0488670bab
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-01-31 22:15:15 +0300

    feat: implement batch writing for addCollectionAndDocuments function and update ProductsProvider to call it

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
 import SHOP_DATA from '../shop-data.js';
 
 export const ProductsContext = createContext({
@@ -9,6 +10,9 @@ export const ProductsContext = createContext({
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   const value = { products };
+  useEffect(() => {
+    addCollectionAndDocuments('categories', SHOP_DATA);
+  }, [])
 
   return (
     <ProductsContext.Provider value={value}>
