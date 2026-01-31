7ecbce597aa46df61be4080c107f373b05b88e7c
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-01-31 22:15:32 +0300

    refactor: remove unused SHOP_DATA import and cleanup useEffect in ProductsProvider

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,7 +1,6 @@
 import { createContext, useState, useEffect } from 'react';
 
 import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
-import SHOP_DATA from '../shop-data.js';
 
 export const ProductsContext = createContext({
   products: [],
@@ -10,9 +9,6 @@ export const ProductsContext = createContext({
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   const value = { products };
-  useEffect(() => {
-    addCollectionAndDocuments('categories', SHOP_DATA);
-  }, [])
 
   return (
     <ProductsContext.Provider value={value}>
