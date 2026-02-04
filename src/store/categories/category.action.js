8324e34d4ce821629ab26ca47d2cd28d1dfad658
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-02-04 19:04:14 +0300

    refactor: update setCategoriesMap to setCategories in Redux actions and reducers.

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,6 @@
 import { CATEGORIS_ACTION_TYPING } from "./category.types";
 import { createAction } from "../../utils/reducer/reducer.utils";
 
-export const setCategoriesMap = (categoriesMap) => {
-  return createAction(CATEGORIS_ACTION_TYPING.SET_CATEGORIS_MAP, categoriesMap);
+export const setCategories = (categoriesArray) => {
+  return createAction(CATEGORIS_ACTION_TYPING.SET_CATEGORIS, categoriesArray);
 };
--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,17 +1,17 @@
 import { CATEGORIS_ACTION_TYPING } from './category.types';
 
 const CATEGORIS_INITAIL_STATE = {
-  categoriesMap: {},
+  categories: [],
 };
 
 export const categoriesReduser = (state = CATEGORIS_INITAIL_STATE, action = {}) => {
   const { type, payload } = action;
 
   switch (type) {
-    case CATEGORIS_ACTION_TYPING.SET_CATEGORIS_MAP:
+    case CATEGORIS_ACTION_TYPING.SET_CATEGORIS:
       return {
         ...state,
-        categoriesMap: payload,
+        categories: payload,
       };
     default:
       return state;
