9436639d6066795887fb11b74f317133a77fe739
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-02-08 00:26:46 +0300

    refactor: update category actions to use async functions and thunk middleware

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,24 @@
-import { CATEGORIS_ACTION_TYPING } from './category.types';
+import { CATEGORIS_ACTION_TYPES } from './category.types';
 import { createAction } from '../../utils/reducer/reducer.utils';
 
-export const setCategories = (categoriesArray) => {
-  return createAction(CATEGORIS_ACTION_TYPING.SET_CATEGORIS, categoriesArray);
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils'
+
+export const fetchCategoriesStart = () => 
+  createAction(CATEGORIS_ACTION_TYPES.FETCH_CATEGORISE_START);
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
+  createAction(CATEGORIS_ACTION_TYPES.FETCH_CATEGORISE_SUCCESS, categoriesArray);
+
+export const fetchCategoriesFailed = (error) =>
+  createAction(CATEGORIS_ACTION_TYPES.FETCH_CATEGORISE_SUCCESS, error);
+
+export const fetchCategoriesAsync = () => async (disparch) => {
+  disparch(fetchCategoriesStart);
+
+  try {
+    const categoriesArray = await getCategoriesAndDocuments('categories');
+    disparch(fetchCategoriesSuccess(categoriesArray));
+  } catch (error) {
+    disparch(fetchCategoriesFailed(error));
+  }
 };
--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,18 +1,21 @@
-import { CATEGORIS_ACTION_TYPING } from './category.types';
+import { CATEGORIS_ACTION_TYPES } from './category.types';
 
 const CATEGORIS_INITAIL_STATE = {
   categories: [],
+  isLoading: false,
+  error: null,
 };
 
 export const categoriesReduser = (state = CATEGORIS_INITAIL_STATE, action = {}) => {
   const { type, payload } = action;
 
   switch (type) {
-    case CATEGORIS_ACTION_TYPING.SET_CATEGORIS:
-      return {
-        ...state,
-        categories: payload,
-      };
+    case CATEGORIS_ACTION_TYPES.FETCH_CATEGORISE_START:
+      return { ...state, isLoading: true };
+    case CATEGORIS_ACTION_TYPES.FETCH_CATEGORISE_SUCCESS:
+      return { ...state, categories: payload, isLoading: false };
+    case CATEGORIS_ACTION_TYPES.FETCH_CATEGORISE_FAILED:
+      return { ...state, error: payload, isLoading: false };
     default:
       return state;
   }
