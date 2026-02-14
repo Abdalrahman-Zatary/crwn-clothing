7f78c8116db87b974676fa2be8b0af8df30b0638
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-02-14 12:16:11 +0300

    add: implement Netlify serverless function for creating payment intents

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -5,22 +5,22 @@ exports.handler = async (event) => {
   try {
     const { amount } = JSON.parse(event.body);
 
-    const paymentIntent = await stripe.paymentIntent.create({
+    const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency: "usd",
-      payment_method_types: ["card"]
+      payment_method_types: ["card"],
     });
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ paymentIntent })
-    }
-  } catch(error) {  
-    console.log(error)
+      body: JSON.stringify({ paymentIntent }),
+    };
+  } catch (error) {
+    console.log(error);
 
     return {
-      status: 400,
-      body: JSON.stringify({ error })
-    }
+      statusCode: 400,
+      body: JSON.stringify({ error }),
+    };
   }
-}
+};
