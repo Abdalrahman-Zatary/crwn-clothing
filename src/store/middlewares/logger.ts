bd5c7bb37b3621af70d2a0ce6e49e4a27742c6ba
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-02-21 21:35:59 +0300

    fix: improve logging output in loggerMiddleware

--- src/store/middlewares/logger.ts
+++ src/store/middlewares/logger.ts
@@ -2,15 +2,17 @@ import { Middleware } from 'redux';
 import { RootState } from '../store';
 
 export const loggerMiddleware: Middleware<{}, RootState> = (store) => (next) => (action) => {
-  if (!action.type) {
+  if (typeof action === 'object' && action !== null && 'type' in action) {
     return next(action);
   }
 
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
+  console.log("type: ", (action as any).type);
+  console.log("payload: ", (action as any).payload);
   console.log("currentState: ", store.getState());
 
-  next(action);
+  const result = next(action);
 
   console.log("next state: ", store.getState());
+
+  return result;
 };
