dc15cb78743a9e04aa59838b0361053f3ec8c7fa
Abdalrahman-Zatary <abd.alrahman.zitre.2005@gmail.com>
2026-02-24 23:53:57 +0300

    refactor: optimize CartItem and CheckoutItem components by using memo for performance

--- src/components/checkout-item/checkout-item.component.tsx
+++ src/components/checkout-item/checkout-item.component.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { selectCartItems } from '../../store/cart/cart.selectors';
